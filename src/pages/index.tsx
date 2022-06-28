bae454bd710e3a2f7a1176592986d81a50ecd47a
landtry <landry@boldist.co>
2022-06-27 19:10:33 -0500

    get random pokemon helper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,19 @@
+import { getOptionsForVote } from "@/utils/getRandomPokemon";
 import { trpc } from "@/utils/trpc";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  const { data, isLoading } = trpc.useQuery(["hello", { text: "Landry" }]);
-
-  if (isLoading) return <h1>Loading...</h1>;
-
-  if (data)
-    return (
-      <h1 className=" text-xl text-slate-200 font-bold">{data.greeting}</h1>
-    );
-
+  const [first, second] = getOptionsForVote();
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <h3 className="text-slate-200">Which pokemon is roundest?</h3>
       <div className="h-6"></div>
       <div className="flex border rounded-xl p-8">
-        <div className="h-16 w-16 bg-red-200"></div>
+        <div className="h-16 w-16 bg-red-200">{first}</div>
         <div className="h-16 w-16 flex justify-center items-center text-slate-200">
           vs
         </div>
-        <div className="h-16 w-16 bg-red-200"></div>
+        <div className="h-16 w-16 bg-red-200">{second}</div>
       </div>
     </div>
   );
